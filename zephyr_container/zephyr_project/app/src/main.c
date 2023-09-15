85a29a0122e820e8d450da14bab8806405e80f4c
Jesse Riemens <j.riemens@alvatechnology.nl>
2023-09-15 23:05:29 +0200

    Add three threads

--- zephyr_container/zephyr_project/app/src/main.c
+++ zephyr_container/zephyr_project/app/src/main.c
@@ -16,13 +16,54 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+#define STACK_SIZE 1024
+
+// Thread declarations
+K_THREAD_STACK_DEFINE(encoder_thread_stack_area, STACK_SIZE);
+K_THREAD_STACK_DEFINE(lcd_thread_stack_area, STACK_SIZE);
+
+struct k_thread encoder_thread;
+struct k_thread lcd_thread;
+
+void encoder_entry(void);
+void lcd_entry(void);
+
 int main(void) {
   printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-  while(1) 
-  {
+  // Create threads
+  k_tid_t encoder_tid = k_thread_create(
+      &encoder_thread, encoder_thread_stack_area, STACK_SIZE, encoder_entry,
+      NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
+
+  k_tid_t lcd_tid =
+      k_thread_create(&lcd_thread, lcd_thread_stack_area, STACK_SIZE, lcd_entry,
+                      NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
+
+  while (1) {
     k_msleep(1000);
-    printk("Hello World!\n");
+    printk("Hello World! (from main thread)\n");
   }
-  
+}
+
+// Incremental encoder thread
+void encoder_entry(void) {
+  while (1) {
+    k_msleep(1234);
+    printk("Encoder thread\n");
+  }
+}
+
+// LCD thread
+void lcd_entry(void) {
+  while (1) {
+    k_msleep(4321);
+    printk("LCD thread\n");
+  }
+}
+
+// GPIO callback
+void gpio_callback(const struct device *dev, struct gpio_callback *cb,
+                   uint32_t pins) {
+  printk("GPIO callback\n");
 }
