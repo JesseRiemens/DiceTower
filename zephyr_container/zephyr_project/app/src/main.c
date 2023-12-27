0463a21db5f7a09df78540b52c74efeaf0aca424
Jesse Riemens <riemensjesse@gmail.com>
2023-12-27 15:16:59 +0100

    Remove unused esp32 overlay

--- zephyr_container/zephyr_project/app/src/main.c
+++ zephyr_container/zephyr_project/app/src/main.c
@@ -5,35 +5,49 @@
  */
 
 #include <string.h>
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/auxdisplay.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(auxdisplay_sample, LOG_LEVEL_DBG);
 
-int main(void)
-{
-	int rc;
-	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(auxdisplay_0));
-	uint8_t data[64];
+int main(void) {
+  int rc;
+  const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(auxdisplay_0));
+  uint8_t data[6] = "Hello";
 
-	if (!device_is_ready(dev)) {
-		LOG_ERR("Auxdisplay device is not ready.");
-		return 0;
-	}
+  if (!device_is_ready(dev)) {
+    LOG_ERR("Auxdisplay device is not ready.");
+    return 0;
+  }
 
-	rc = auxdisplay_cursor_set_enabled(dev, true);
+  rc = auxdisplay_cursor_set_enabled(dev, false);
 
-	if (rc != 0) {
-		LOG_ERR("Failed to enable cursor: %d", rc);
-	}
+  if (rc != 0) {
+    LOG_ERR("Failed to enable cursor: %d", rc);
+  }
 
-	snprintk(data, sizeof(data), "Hello world from %s", CONFIG_BOARD);
-	rc = auxdisplay_write(dev, data, strlen(data));
+  // snprintk(data, sizeof(data), "Hello world from %s", CONFIG_BOARD);
+  while (true) {
 
-	if (rc != 0) {
-		LOG_ERR("Failed to write data: %d", rc);
-	}
-	return 0;
+    rc = auxdisplay_write(dev, data, strlen(data));
+
+    if (rc != 0) {
+      LOG_ERR("Failed to write data: %d", rc);
+    }
+    LOG_INF("Sent data: %s", data);
+
+    // rc =
+    //     auxdisplay_cursor_position_set(dev, AUXDISPLAY_POSITION_ABSOLUTE, 0,
+    //     0);
+    // if (rc != 0) {
+    //   LOG_ERR("Failed to set cursor position: %d", rc);
+    // }
+
+    k_sleep(K_MSEC(1000));
+  }
+
+  LOG_INF("Sample application has completed.");
+  return 0;
 }
